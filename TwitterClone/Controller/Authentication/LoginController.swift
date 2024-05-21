76bcb4e880804293b107332f3b1de6f10711aff8
Justin Hold <36420615+jhold1982@users.noreply.github.com>
2024-05-21 12:27:16 -0500

    Updated views

--- TwitterClone/Controller/Authentication/LoginController.swift
+++ TwitterClone/Controller/Authentication/LoginController.swift
@@ -11,6 +11,7 @@ import UIKit
 class LoginController: UIViewController {
 	
 	// MARK: - Properties
+	// Twitter Logo
 	private let logoImageView: UIImageView = {
 		let imageView = UIImageView()
 		imageView.contentMode = .scaleAspectFit
@@ -19,6 +20,7 @@ class LoginController: UIViewController {
 		return imageView
 	}()
 	
+	// Email Text Field Container View
 	private lazy var emailContainerView: UIView = {
 		
 		let image = #imageLiteral(resourceName: "ic_mail_outline_white_2x-1")
@@ -27,6 +29,7 @@ class LoginController: UIViewController {
 		return view
 	}()
 	
+	// Password Text Field Container View
 	private lazy var passwordContainerView: UIView = {
 		
 		let image = #imageLiteral(resourceName: "ic_lock_outline_white_2x")
@@ -35,15 +38,37 @@ class LoginController: UIViewController {
 		return view
 	}()
 	
+	// Email Text Field
 	private let emailTextField: UITextField = {
 		let textField = Utilities().textField(withPlaceholder: "Email")
 		return textField
 	}()
 	
+	// Password Text Field
 	private let passwordTextField: UITextField = {
 		let textField = Utilities().textField(withPlaceholder: "Password")
 		textField.isSecureTextEntry = true
 		return textField
+	}()
+	
+	// Login Button
+	private let loginButton: UIButton = {
+		let button = UIButton(type: .system)
+		button.setTitle("Log in", for: .normal)
+		button.setTitleColor(.twitterBlue, for: .normal)
+		button.backgroundColor = .white
+		button.heightAnchor.constraint(lessThanOrEqualToConstant: 50).isActive = true
+		button.layer.cornerRadius = 5
+		button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
+		button.addTarget(self, action: #selector(handleLogin), for: .touchUpInside)
+		return button
+	}()
+	
+	// Dont Have Account Button
+	private let dontHaveAccountButton: UIButton = {
+		let button = Utilities().attributedButton("Don't have an account?", " Sign Up")
+		button.addTarget(self, action: #selector(handleShowSignUp), for: .touchUpInside)
+		return button
 	}()
 	
     // MARK: - View Did Load
@@ -64,17 +89,46 @@ class LoginController: UIViewController {
 		
 		logoImageView.setDimension(width: 150, height: 150)
 		
-		let stack = UIStackView(arrangedSubviews: [emailContainerView, passwordContainerView])
+		let stack = UIStackView(
+			arrangedSubviews: [
+				emailContainerView,
+				passwordContainerView,
+				loginButton
+			]
+		)
 		stack.axis = .vertical
-		stack.spacing = 8
+		stack.spacing = 20
+		stack.distribution = .fillEqually
 		
 		view.addSubview(stack)
+		
 		stack.anchor(
 			top: logoImageView.bottomAnchor,
 			left: view.leftAnchor,
 			right: view.rightAnchor,
-			paddingLeft: 16,
-			paddingRight: 16
+			paddingLeft: 32,
+			paddingRight: 32
 		)
+		
+		view.addSubview(dontHaveAccountButton)
+		
+		dontHaveAccountButton.anchor(
+			left: view.leftAnchor,
+			bottom: view.safeAreaLayoutGuide.bottomAnchor,
+			right: view.rightAnchor,
+			paddingLeft: 40,
+			paddingRight: 40
+		)
+	}
+	
+	
+	// MARK: - @OBJC Functions
+	@objc func handleLogin() {
+		//
+	}
+	
+	@objc func handleShowSignUp() {
+		let controller = RegistrationController()
+		navigationController?.pushViewController(controller, animated: true)
 	}
 }
--- TwitterClone/Controller/Authentication/RegistrationController.swift
+++ TwitterClone/Controller/Authentication/RegistrationController.swift
@@ -12,6 +12,82 @@ class RegistrationController: UIViewController {
 	
 	// MARK: - Properties
 	
+	// Plus Photo Button + Image
+	private let plusPhotoButton: UIButton = {
+		let button = UIButton(type: .system)
+		button.setImage(UIImage(named: "plus_photo"), for: .normal)
+		button.tintColor = .white
+		button.addTarget(self, action: #selector(handleAddProfilePhoto), for: .touchUpInside)
+		return button
+	}()
+	
+	// Email Text Field Container View
+	private lazy var emailContainerView: UIView = {
+		
+		let image = #imageLiteral(resourceName: "ic_mail_outline_white_2x-1")
+		let view = Utilities().inputContainerView(withImage: image, textField: emailTextField)
+		
+		return view
+	}()
+	
+	// Password Text Field Container View
+	private lazy var passwordContainerView: UIView = {
+		
+		let image = #imageLiteral(resourceName: "ic_lock_outline_white_2x")
+		let view = Utilities().inputContainerView(withImage: image, textField: passwordTextField)
+		
+		return view
+	}()
+	
+	// Full Name Text Field Container View
+	private lazy var fullNameContainerView: UIView = {
+		
+		let image = #imageLiteral(resourceName: "ic_person_outline_white_2x")
+		let view = Utilities().inputContainerView(withImage: image, textField: fullNameTextField)
+		
+		return view
+	}()
+	
+	// Username Text Field Container View
+	private lazy var usernameContainerView: UIView = {
+		
+		let image = #imageLiteral(resourceName: "ic_person_outline_white_2x")
+		let view = Utilities().inputContainerView(withImage: image, textField: usernameTextField)
+		
+		return view
+	}()
+	
+	// Email Text Field
+	private let emailTextField: UITextField = {
+		let textField = Utilities().textField(withPlaceholder: "Email")
+		return textField
+	}()
+	
+	// Password Text Field
+	private let passwordTextField: UITextField = {
+		let textField = Utilities().textField(withPlaceholder: "Password")
+		textField.isSecureTextEntry = true
+		return textField
+	}()
+	
+	// Full Name Text Field
+	private let fullNameTextField: UITextField = {
+		let textField = Utilities().textField(withPlaceholder: "Full Name")
+		return textField
+	}()
+	
+	// Username Text Field
+	private let usernameTextField: UITextField = {
+		let textField = Utilities().textField(withPlaceholder: "Username")
+		return textField
+	}()
+	
+	// Already have an Account? - Back Button
+	private let alreadyHaveAccountButton: UIButton = {
+		let button = Utilities().attributedButton("Already have an account?", " Log in")
+		button.addTarget(self, action: #selector(handleShowLogin), for: .touchUpInside)
+		return button
+	}()
 	
 	
 	// MARK: - View Did Load
@@ -25,6 +101,25 @@ class RegistrationController: UIViewController {
 	// MARK: - Functions
 	func configureUI() {
 		view.backgroundColor = .twitterBlue
+		
+		view.addSubview(alreadyHaveAccountButton)
+		alreadyHaveAccountButton.anchor(
+			left: view.leftAnchor,
+			bottom: view.safeAreaLayoutGuide.bottomAnchor,
+			right: view.rightAnchor,
+			paddingLeft: 40,
+			paddingRight: 40
+		)
+	}
+	
+	// MARK: - @OBJC Functions
+	@objc func handleShowLogin() {
+		let controller = LoginController()
+		navigationController?.popViewController(animated: true)
+	}
+	
+	@objc func handleAddProfilePhoto() {
+		//
 	}
 }
 
